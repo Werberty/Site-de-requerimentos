a27df6e0b459f1b44510571bb01cfd97d8eeade2
Werberty <werberty.alexandre@gmail.com>
2022-06-14 21:33:28 -0300

    Começando a fazer o app login

--- projeto/settings.py
+++ projeto/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'autentificacao.apps.AutentificacaoConfig'
 ]
 
 MIDDLEWARE = [
