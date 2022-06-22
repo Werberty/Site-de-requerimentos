a8649b94424a3521623041abdfa14826c00daf6e
Werberty <werberty.alexandre@gmail.com>
2022-06-22 16:47:45 -0300

    Recriando todo o template par bootstrap: Etapa login

--- projeto/urls.py
+++ projeto/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('requerimento/', include('requerimentos.urls')),
+    path('', include('requerimentos.urls')),
     path('auth/', include('autentificacao.urls')),
 ]
